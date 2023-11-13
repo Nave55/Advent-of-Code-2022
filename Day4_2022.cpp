50eab712242cdd970ae53fe31aff6caee23c9e13
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2023-11-13 00:25:05 -0700

    Add files via upload

---
+++ Day4_2022.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution(const string &s, const char &del);
+
+int main() {
+    ifstream myfile ("C:/Users/navez/Anaconda Proj/C++ Projects/AoC/Advent_2022_Files/Day4.txt");
+    stringstream instream;
+    instream << myfile.rdbuf(); 
+    string con = instream.str();
+    myfile.close();
+
+    con = regex_replace(con, regex("-"), ",");
+    con = regex_replace(con, regex("\n"), ",");
+
+    solution(con,',');
+}
+
+void solution(const string &s, const char &del) {
+    int sum {0}, sum2 {0};
+    vector<int> vec {};
+    
+    stringstream ss {s};
+    string word;
+    while (!ss.eof()) {
+        getline(ss, word, del);
+        vec.push_back(stoi(word));
+        if (vec.size() == 4) {
+            if ((vec.at(0) >= vec.at(2) && vec.at(1) <= vec.at(3)) || 
+            (vec.at(2) >= vec.at(0) && vec.at(3) <= vec.at(1))) sum++;
+
+            if ((vec.at(0) >= vec.at(2) && vec.at(0) <= vec.at(3)) || 
+            (vec.at(1) >= vec.at(2) && vec.at(1) <= vec.at(3)) || 
+            (vec.at(2) >= vec.at(0) && vec.at(2) <= vec.at(1)) || 
+            (vec.at(3) >= vec.at(1) && vec.at(3) <= vec.at(0))) sum2++;
+
+            vec.clear();
+        }
+    }
+    cout << sum << "\n" << sum2 << "\n";
+}
