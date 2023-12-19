23ec4fcaaa284442a36b6e0ea70da502cd54a399
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2023-12-19 00:42:36 -0700

    Update Day7_2022.cpp

--- c++/Day7_2022.cpp
+++ c++/Day7_2022.cpp
@@ -1,43 +1,17 @@
 #include <bits/stdc++.h>
+#include "tools.h"
 
 using namespace std;
 
-void solution(const string &con, const char &del);
-string parsefile(const char &del);
-
-template <typename T>
-T strConcat(const vector<T> &arr);
-
 int main() {
-    string con = parsefile('\n');
-    solution(con,'\n');
-}
-
-string parsefile(const char &del) {
     string str;
     ifstream myfile ("C:/Users/navez/Anaconda Proj/C++ Projects/AoC/Advent_2022_Files/Day7.txt");
     stringstream instream;
     instream << myfile.rdbuf(); 
     string con = instream.str();
     myfile.close();
-    
-    stringstream ss {con};
-    string word;
-    while (!ss.eof()) {
-        getline(ss, word, del);
-        if (word.find('$') == 0) word.replace(0,2,"");
-        if (word != "ls" && word[0] != 'd' && ! isdigit(word[0])) str += word += '\n';
-        if (isdigit(word[0])) str += word.substr(0,word.find(' ')) += '\n';
-    }
-    return str;
 
-}
-
-void solution(const string &con, const char &del) {
-    struct Directory {
-        string path;
-        int size {0};
-    };
+    struct Directory {string path; int size {0};};
     vector<Directory> dir;
     vector<string> tmp_dir;
     set<string> dir_names;
@@ -45,53 +19,35 @@ void solution(const string &con, const char &del) {
     string word;
 
     while (!ss.eof()) {
-        getline(ss, word, del);
-        if (word[0] == 'c' && word[3] != '.') {
-            tmp_dir.emplace_back(word.substr(3));
-            auto str = strConcat(tmp_dir);
-            dir_names.insert(str);         
+        getline(ss, word, '\n');
+        if (word[2] == 'c' && word[5] != '.') {
+            tmp_dir.push_back(word.substr(3));
+            dir_names.insert(strConcat(tmp_dir, '-'));         
         }
-        if (word[3] == '.') tmp_dir.pop_back();
-        if (isdigit(word[0])) {
-            auto num = stoi(word.substr(0,word.find(' ')));
-            auto str = strConcat(tmp_dir);
-            dir.emplace_back(Directory{str, num});
-        }
-    }
-    
-    int sum {0};
-    vector<int> ttl;
-    for (auto i: dir_names) {
-        for (auto j: dir) {
-            if (j.path.find(i) < j.path.size()) {
-                sum += j.size;
-            }   
-        }
-        if (sum > 0) {
-            ttl.emplace_back(sum);
-            sum = 0;
+        else if (word[5] == '.') tmp_dir.pop_back();
+        else if (isdigit(word[0])) {
+            auto p = strConcat(tmp_dir, '-');
+            auto s  = stoi(word.substr(0,word.find(' ')));
+            dir.push_back(Directory{p, s});
         }
     }
 
+    vector<int> ttl;
+    for (auto i: dir_names) {
+        int sum {0};
+        for (auto j: dir) {
+            if (j.path.find(i) < j.path.size()) sum += j.size;
+        }
+        ttl.push_back(sum);
+    }
+    
+    int sum {0};
     sort(ttl.begin(), ttl.end());
     for (auto i: ttl) {
-        if (i <= 100000) sum += i;
-        if (i >= (ttl.at(ttl.size() - 1) - 40000000)) {
+        if (i <= 100'000) sum += i;
+        if (i >= (ttl.at(ttl.size() - 1) - 40'000'000)) {
             cout << "Part 1: " << sum << '\n' << "Part 2: " << i << endl;
             break;
         }
     }
-}
-
-template <typename T>
-T strConcat(const vector<T> &arr) {
-    string str {""};
-    for (size_t i {0}; i < arr.size(); ++i) {
-        if (i < arr.size() - 1) {
-            str += arr.at(i);
-            str += "-";
-        }
-        else str += arr.at(i);
-    }
-    return str;
 }
