47c058317e2b2e0266ff740875bbbce456188e52
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2023-12-19 00:43:18 -0700

    Add files via upload

---
+++ c++/tools.h
@@ -0,0 +1,33 @@
+#ifndef _tools_h_
+#define _tools_h_
+
+#include <ostream>
+#include <vector>
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &arr) {
+    os << "[";
+    for (size_t i {0}; i < arr.size(); ++i) {
+        os << arr[i];
+        if (i < arr.size() - 1) {
+            os << ",";
+        }
+    }
+    os << "]\n";
+    return os;
+}
+
+template <typename T>
+T strConcat(const std::vector<T> &arr, const char &delim) {
+    std::string str {""};
+    for (size_t i {0}; i < arr.size(); ++i) {
+        if (i < arr.size() - 1) {
+            str += arr.at(i);
+            str += delim;
+        }
+        else str += arr.at(i);
+    }
+    return str;
+}
+
+#endif
