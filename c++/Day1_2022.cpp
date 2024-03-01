6ed76f155a23aed5a09dc62560b7d874bf3a1591
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-03-01 02:07:27 -0700

    Update Day1_2022.cpp

--- c++/Day1_2022.cpp
+++ c++/Day1_2022.cpp
@@ -1,32 +1,22 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
-void solution(const string &s, const char &del);
-
 int main() {
-    ifstream myfile ("C:/Users/navez/Anaconda Proj/C++ Projects/AoC/Advent_2022_Files/Day1.txt");
-    stringstream instream;
-    instream << myfile.rdbuf(); 
-    string con = instream.str();
-    myfile.close();
-
-    solution(con,'\n');
-}
-
-void solution(const string &s, const char &del) {
-    int sum {0};
-    vector<int> vec {};
-    stringstream ss {s};
-    string word;
-    while (!ss.eof()) {
-        getline(ss, word, del);
-        if (word.length() > 0) sum += stoi(word);
-        else {
-            vec.push_back(sum);
-            sum = 0;
+    std::vector<int> vec;
+    std::ifstream myfile ("C:/Users/navez/Cpp_Projects/AoC/Advent_2022_Files/Day1.txt");
+    if (myfile.is_open()) {
+        std::string line;
+        int ttl {0};
+        while (std::getline(myfile, line)) {
+            if (line != "") ttl += std::stoi(line);
+            else {
+                vec.push_back(std::move(ttl));
+                ttl = 0;
+            }
         }
-    }
-    nth_element(vec.begin(), vec.begin() + 1, vec.end(), greater<int> {});
-    cout << vec.at(0) << "\n" << vec.at(0) + vec.at(1) + vec.at(2) << "\n";
+        myfile.close();
+    } 
+    std::partial_sort(vec.begin(), vec.begin() + 3, vec.end(), std::greater<int> {});
+    auto part1 = vec.at(0);
+    auto part2 = std::accumulate(vec.begin(), vec.begin() + 3, 0, [](int a, int b) {return a + b;});
+    printf("Part 1: %d\nPart 2: %d", part1, part2);
 }
