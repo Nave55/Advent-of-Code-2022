44e53d314389ae0058c7e754510903f14200d61c
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-03-04 00:41:28 -0700

    Update tools.h

--- c++/tools.h
+++ c++/tools.h
@@ -1,8 +1,20 @@
+#pragma once
 #ifndef _tools_h_
 #define _tools_h_
 
-#include <ostream>
 #include <vector>
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+#include <ostream>
+
+typedef std::vector<int> vi;
+typedef std::vector<std::vector<int>> vvi;
+typedef std::vector<std::string> vs;
+typedef std::vector<std::vector<std::string>> vvs;
+typedef std::vector<char> vc;
+typedef std::vector<std::vector<char>> vvc;
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &arr) {
@@ -18,7 +30,7 @@ std::ostream &operator<<(std::ostream &os, const std::vector<T> &arr) {
 }
 
 template <typename T>
-T strConcat(const std::vector<T> &arr, const char &delim) {
+T join(const std::vector<T> &arr, const char &delim = ',') {
     std::string str {""};
     for (size_t i {0}; i < arr.size(); ++i) {
         if (i < arr.size() - 1) {
@@ -28,6 +40,27 @@ T strConcat(const std::vector<T> &arr, const char &delim) {
         else str += arr.at(i);
     }
     return str;
+}
+
+template <typename T>
+float avgVal(const std::vector<T> &arr) {
+    if (arr.size() != 0)
+        return (float) std::accumulate(arr.begin(), arr.end(), 0) / arr.size();
+    return 0;
+}
+
+template <typename T>
+T minVal(const std::vector<T> &arr) {
+    if (arr.size() != 0) 
+        return *std::ranges::min_element(arr);
+    return 0;
+}
+
+template <typename T>
+T maxVal(const std::vector<T> &arr) {
+    if (arr.size() != 0) 
+        return *std::ranges::max_element(arr);
+    return 0;
 }
 
 #endif
