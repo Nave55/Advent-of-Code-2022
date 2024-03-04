3cdb58bc3b0e2b96cf2348823a2ccfe9cc1d3543
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-03-04 00:40:51 -0700

    Update Day7_2022.cpp

--- c++/Day7_2022.cpp
+++ c++/Day7_2022.cpp
@@ -1,52 +1,45 @@
+#include "tl/getlines.hpp"
+#include "tl/to.hpp"
 #include <bits/stdc++.h>
 #include "tools.h"
 
-using namespace std;
-
 int main() {
-    ifstream myfile ("C:/Users/navez/Anaconda Proj/C++ Projects/AoC/Advent_2022_Files/Day7.txt");
-    stringstream instream;
-    instream << myfile.rdbuf(); 
-    string con = instream.str();
-    myfile.close();
+    std::ifstream file("C:/Users/navez/Cpp_Projects/AoC/Advent_2022_Files/Day7.txt");
+    auto lines = tl::views::getlines(file) | tl::to<std::vector>();
 
-    struct Directory {string path; int size {0};};
-    vector<Directory> dir;
-    vector<string> tmp_dir;
-    set<string> dir_names;
-    stringstream ss {con};
-    string word;
+    struct Directory {std::string path; int size {0};};
+    std::vector<Directory> dir;
+    std::vector<std::string> tmp_dir;
+    std::set<std::string> dir_names;
 
-    while (!ss.eof()) {
-        getline(ss, word, '\n');
+    for (auto word: lines) {
         if (word[2] == 'c' && word[5] != '.') {
-            tmp_dir.push_back(word.substr(3));
-            dir_names.insert(strConcat(tmp_dir, '-'));         
+            tmp_dir.push_back(std::move(word.substr(5)));
+            dir_names.insert(join(tmp_dir, '-'));         
         }
         else if (word[5] == '.') tmp_dir.pop_back();
         else if (isdigit(word[0])) {
-            auto p = strConcat(tmp_dir, '-');
-            auto s  = stoi(word.substr(0,word.find(' ')));
-            dir.push_back(Directory{p, s});
+            auto p = join(tmp_dir, '-');
+            auto s  = std::stoi(word.substr(0,word.find(' ')));
+            dir.push_back(Directory{std::move(p), std::move(s)});
         }
     }
-
-    vector<int> ttl;
+        
+    std::vector<int> ttl;
     for (auto i: dir_names) {
         int sum {0};
         for (auto j: dir) {
             if (j.path.find(i) < j.path.size()) sum += j.size;
         }
-        ttl.push_back(sum);
+        ttl.push_back(std::move(sum));
     }
-    
-    int sum {0};
-    sort(ttl.begin(), ttl.end());
-    for (auto i: ttl) {
-        if (i <= 100'000) sum += i;
-        if (i >= (ttl.at(ttl.size() - 1) - 40'000'000)) {
-            cout << "Part 1: " << sum << '\n' << "Part 2: " << i << endl;
-            break;
-        }
-    }
+
+    auto filter1 = ttl | std::ranges::views::filter([](auto x) {return x <= 100'000;});
+    auto filter2 = ttl | std::ranges::views::filter([ttl](auto x) {return x >= ttl[0] - 40'000'000;})
+                       | tl::to<std::vector>();
+
+    auto pt1 = std::accumulate(filter1.begin(), filter1.end(), 0);
+    auto pt2 = minVal(filter2);
+
+    printf("Part 1: %d\nPart 2: %d", pt1, pt2);
 }
