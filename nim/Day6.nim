a7ecfa8ff4ea57a290033b55dbf706a9abd092c2
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-03-07 22:44:27 -0700

    Update Day6.nim

--- nim/Day6.nim
+++ nim/Day6.nim
@@ -1,24 +1,16 @@
 import sets, strformat
 
-proc solution(): (int, int) =
-    var 
-        ttl1 = 0
-        ttl2 = 0
+proc solution(seqlen: int): int =
     let file = open("input/Day6.txt");
     defer: file.close()
 
     for i in file.lines():
-        for j in 0..<len(i) - 3:
-            if len(i[j..j + 3].toHashSet()) == 4:
-                ttl1 += j + 4
-                break
-        for j in 0..<len(i) - 13:
-            if len(i[j..j + 13].toHashSet()) == 14:
-                ttl2 += j + 14
-                break
-
-    return (ttl1, ttl2)
+        for j in 0..<len(i) - seqlen - 1:
+            if len(i[j..j + seqlen - 1].toHashSet()) == seqlen:
+                return j + seqlen
         
-let ttl = solution()
+let 
+    ttl1 = solution(4)
+    ttl2 = solution(14)
 
-echo &"Solution 1: {ttl[0]}\nSolution 2: {ttl[1]}"
+echo &"Solution 1: {ttl1}\nSolution 2: {ttl2}"
