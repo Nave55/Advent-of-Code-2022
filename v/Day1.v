c858d82533e393e7ee68a403641aa528f0ed5703
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-05-02 12:41:31 -0600

    Update Day1.v

--- v/Day1.v
+++ v/Day1.v
@@ -1,1 +1,19 @@
+import os
+import arrays
 
+fn main() {
+	mut ttl := 0
+	mut arr := []int{}
+	lines := os.read_lines("input/Day1_2022.txt")!
+	for i in lines {
+		if i.int() != 0 {
+			ttl += i.int()
+		}
+		else {
+			arr << ttl 
+			ttl = 0
+		}
+	}
+	arr.sort(b < a)
+	println("Part 1: ${arr[0]}\nPart 2: ${arrays.sum[int](arr[0..3])!}")	
+}
