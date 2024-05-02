01d2f42633f4aac4ced3ecd5654367cc5e6b4ca8
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-05-02 12:59:42 -0600

    Update Day2.v

--- v/Day2.v
+++ v/Day2.v
@@ -8,12 +8,12 @@ fn main() {
 	mut pt1, mut pt2 := 0, 0
 
 	map1 := {"A X": 4, "A Y": 8, "A Z": 3,
-        					 "B X": 1, "B Y": 5, "B Z": 9,
-        					 "C X": 7, "C Y": 2, "C Z": 6}
+        	 "B X": 1, "B Y": 5, "B Z": 9,
+		 "C X": 7, "C Y": 2, "C Z": 6}
 	
 	map2 := {"A X": "A Z", "A Y": "A X", "A Z": "A Y",
-        					    "B X": "B X", "B Y": "B Y", "B Z": "B Z",   
-                                "C X": "C Y", "C Y": "C Z", "C Z": "C X"}
+        	 "B X": "B X", "B Y": "B Y", "B Z": "B Z",   
+                 "C X": "C Y", "C Y": "C Z", "C Z": "C X"}
 
 	for i in arr {
 		pt1 += map1[i]
