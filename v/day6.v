bac53c4a515753d842fd1eb3c73eb09684cfd6c3
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-05-05 21:01:28 -0600

    Add files via upload

---
+++ v/day6.v
@@ -0,0 +1,18 @@
+import os
+import arrays
+
+fn main() {
+	input := os.read_lines("input/Day6_2022.txt")![0].split("")
+	pt1 := solution(4, input)!
+	pt2 := solution(14, input)!
+	println("Part 1: ${pt1}\nPart 2: ${pt2}")
+}
+
+fn solution(window int, input []string)! int {
+	for i in 0..input.len - window - 1 {
+		if arrays.distinct(input[i..i + window]).len == window {
+			return i + window
+		}
+	}
+	return error("No unique solution found")
+}
