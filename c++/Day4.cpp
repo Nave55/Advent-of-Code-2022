e2c21800184c8dc2cadc3b33e31a3c096a110fe2
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-07-22 21:55:51 -0600

    Update Day4.cpp

--- c++/Day4.cpp
+++ c++/Day4.cpp
@@ -9,7 +9,7 @@ int main() {
     std::ifstream file ("C:/Users/navez/Cpp_Projects/AoC/Advent_2022_Files/Day4.txt");
     auto lines = tl::views::getlines(file) | tl::to<std::vector>();
     for (size_t i {0}; i < lines.size(); i++) std::replace(lines[i].begin(), lines[i].end(), '-', ',');
-   auto s = split(lines, ',');
+    auto s = split(lines, ',');
 
    int sum {0}, sum2 {0};
    for (size_t i {0}; i < s.size() - 3; i += 4) {
