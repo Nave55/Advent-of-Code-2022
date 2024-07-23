c132524e73137d30ce02f66d64c7a5d0dd253cbf
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-07-22 23:29:27 -0600

    Update Day6.cpp

--- c++/Day6.cpp
+++ c++/Day6.cpp
@@ -5,18 +5,19 @@
 #include "tl/getlines.hpp"
 #include "tl/to.hpp"
 
-auto solution(const std::string &lines, size_t window) -> int;
+auto solution(const std::string &con, size_t window) -> int;
 
 int main() {
     std::ifstream file ("C:/Users/navez/Cpp_Projects/AoC/Advent_2022_Files/Day6.txt");
-    auto lines = tl::views::getlines(file) | tl::to<std::vector<std::string>>();
-    std::printf("Part 1: %d\nPart 2: %d\n", solution(lines[0], 4), solution(lines[0], 14));
+    std::string line;
+    file >> line;
+    std::printf("Part 1: %d\nPart 2: %d\n", solution(line, 4), solution(line, 14));
 }
 
-auto solution(const std::string &lines, size_t window) -> int {
-    for (size_t i {0}; i < lines.size() - window + 1; i++) {
+auto solution(const std::string &line, size_t window) -> int {
+    for (size_t i {0}; i < line.size() - window + 1; i++) {
         std::set<char> s {};
-        for (size_t j {i}; j < i + window; j++) s.insert(lines[j]);
+        for (size_t j {i}; j < i + window; j++) s.insert(line[j]);
         if (s.size() == window) return i + window;
     }
     return 0;   
