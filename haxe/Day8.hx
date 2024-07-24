13dd6f972e019642d317259f1cc8b10898c6003f
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-07-24 16:10:15 -0600

    Update Day8.hx

--- haxe/Day8.hx
+++ haxe/Day8.hx
@@ -8,20 +8,16 @@ using Lambda;
 class Day8 {
     static function main() {
         var s: AS = [for (i in sys.io.File.getContent('Advent Files_2022/Day8.txt').split('\n')) trim(i)];
-        solution1(s);
-    }
-
-    static function solution1(s: AS) {
         var count:Float = (s.length * 4) - 4;
         var scores: AI = []; 
 
         for (rows in 1...s.length - 1) {
             for (cols in 1...s.length - 1) {
                 var pos = parseInt(s[rows].charAt(cols));
-                var left: AI =   [for (i in 1...cols + 1)         parseInt(s[rows].charAt(cols - i))];
-                var right: AI =  [for (i in 1...s.length - cols)  parseInt(s[rows].charAt(cols + i))];
-                var top: AI =    [for (i in 1...rows + 1)         parseInt(s[rows - i].charAt(cols))];
-                var bottom: AI = [for (i in 1...s.length - rows)  parseInt(s[rows + i].charAt(cols))];
+                var left:   AI =   [for (i in 1...cols + 1)         parseInt(s[rows].charAt(cols - i))];
+                var right:  AI =   [for (i in 1...s.length - cols)  parseInt(s[rows].charAt(cols + i))];
+                var top:    AI =   [for (i in 1...rows + 1)         parseInt(s[rows - i].charAt(cols))];
+                var bottom: AI =   [for (i in 1...s.length - rows)  parseInt(s[rows + i].charAt(cols))];
 
                 if (maxVal(left) < pos || maxVal(right) < pos || maxVal(top) < pos || maxVal(bottom) < pos) count++;
                 
