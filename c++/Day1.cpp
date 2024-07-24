dbb97d77bed214bd99569c6f2de018caad2a85b0
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-07-24 01:40:25 -0600

    Update Day1.cpp

--- c++/Day1.cpp
+++ c++/Day1.cpp
@@ -1,22 +1,23 @@
-#include <bits/stdc++.h>
+#include "tools.h"
+#include <tl/getlines.hpp>
+#include <tl/to.hpp>
+#include <fstream>
 
 int main() {
+    std::ifstream file ("C:/Users/navez/Cpp_Projects/AoC/Advent_2022_Files/Day1.txt");
+    auto lines = tl::views::getlines(file) | tl::to<std::vector<std::string>>();
+
     std::vector<int> vec;
-    std::ifstream myfile ("C:/Users/navez/Cpp_Projects/AoC/Advent_2022_Files/Day1.txt");
-    if (myfile.is_open()) {
-        std::string line;
-        int ttl {0};
-        while (std::getline(myfile, line)) {
-            if (line != "") ttl += std::stoi(line);
-            else {
-                vec.push_back(std::move(ttl));
-                ttl = 0;
-            }
+    int ttl {0};
+    for (const auto &i : lines) {
+        if (i != "") ttl += std::stoi(i);
+        else {
+            vec.emplace_back(ttl);
+            ttl = 0;
         }
-        myfile.close();
-    } 
+    }
     std::partial_sort(vec.begin(), vec.begin() + 3, vec.end(), std::greater<int> {});
     auto part1 = vec.at(0);
-    auto part2 = std::accumulate(vec.begin(), vec.begin() + 3, 0, [](int a, int b) {return a + b;});
+    auto part2 = std::accumulate(vec.begin(), vec.begin() + 3, 0);
     printf("Part 1: %d\nPart 2: %d", part1, part2);
 }
