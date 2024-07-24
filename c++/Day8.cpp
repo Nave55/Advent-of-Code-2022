a5ee0e9977b5e5c84e993069e84d3a52449026e7
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-07-24 02:50:50 -0600

    Update Day8.cpp

--- c++/Day8.cpp
+++ c++/Day8.cpp
@@ -1,139 +1,46 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-
-void solution1(const string &rows, const string & columns, const char &del, const int &len);
-void parsefile(string &rows, string &columns, int &len);
+#include "tools.h"
+#include <tl/getlines.hpp>
+#include <tl/to.hpp>
+#include <boost/algorithm/string.hpp>
+#include <print>
+#include <iostream>
+#include <fstream>
 
 int main() {
-    string rows, columns;
-    int len {1};
-    parsefile(rows, columns, len);
-    solution1(rows, columns, '\n', len);
-}
+    std::ifstream file ("C:/Users/navez/Cpp_Projects/AoC/Advent_2022_Files/Day8.txt");
+    auto s = tl::views::getlines(file) | tl::to<std::vector<std::string>>();
 
-void parsefile(string &rows, string &columns, int &len) {
-    ifstream myfile ("C:/Users/navez/Anaconda Proj/C++ Projects/AoC/Advent_2022_Files/Day8.txt");
-    stringstream instream;
-    instream << myfile.rdbuf(); 
-    string con = instream.str();
-    myfile.close();
-    for (auto i: con) {
-        rows += i;
-        if (i == '\n') ++len;
-    }
+    int count = (s.size() * 2 + s[0].size() * 2) - 4;
+    vi scores;
 
-    for (int i {0}; i < len; ++i) {
-        if (i != 0) columns += '\n';
-        stringstream ss {con};
-        string word;
-        while (!ss.eof()) {
-            getline(ss, word,'\n');
-            columns += word[i];
-        }
-    }
-}
+    for (size_t rows = 1; rows < s.size() - 1; rows++) {
+        for (size_t cols = 1; cols < s[cols].size() - 1; cols++) {
+            int pos = (int) s[rows][cols];
+            vi left, right, top, bottom;
 
-void solution1(const string &rows, const string & columns, const char &del, const int &len) {
-    vvi matrix; 
-    for (int i {0}; i < len; ++i) {
-        vi tmp_mat {};
-        for (int j {0}; j < len; ++j) {
-            tmp_mat.emplace_back(1);
-        }
-        matrix.emplace_back(tmp_mat);
-    }
-    auto matrix2 {matrix};
+            for (size_t i = 1; i < cols + 1; i++) left.emplace_back((int) s[rows][cols - i]);
+            for (size_t i = 1; i < s.size() - cols; i++) right.emplace_back((int) s[rows][cols + i]);
+            for (size_t i = 1; i < rows + 1; i++) top.emplace_back((int) s[rows - i][cols]);
+            for (size_t i = 1; i < s.size() - rows; i++) bottom.emplace_back((int) s[rows + i][cols]);
 
-    stringstream ss {rows}, ss_c {columns};
-    string word, word_c;
-    int cnt {0};
+            if (maxVal(left) < pos || maxVal(right) < pos || maxVal(top) < pos || maxVal(bottom) < pos) count++;
 
-    while (!ss.eof()) {
-        getline(ss, word, del);
-        auto r_word {word};
-        reverse(r_word.begin(), r_word.end());
-        getline(ss_c, word_c, del);
-        auto r_word_c {word_c};
-        reverse(r_word_c.begin(), r_word_c.end());
-        int pos {len - 2};
-
-        for (int j {1}; j < len - 1; ++j) {
-            bool visible {false}, visible_r {false}, visible_c {false}, visible_c_r {false};
-            bool broke {false}, broke_r {false}, broke_c {false}, broke_c_r {false};
-            int final_pos {0}, final_pos_r {0}, final_pos_c {0}, final_pos_c_r {0};
-            for (int i {j+1}; i < len; ++i) {
-                if (cnt == 0 || cnt == len - 1) break;
-
-                if (stoi(string(1,word[j])) > (stoi(string(1,word[i])))) {
-                    visible = true;
-                    if (! broke) final_pos = i - j;
+            int score {1};
+            for (const auto &lst: {left, right, top ,bottom}) {
+                int tracker {0};
+                for (size_t i {0}; i < lst.size(); i++) {
+                    if (lst[i] < pos) tracker++;
+                    else if (lst[i] == pos) {
+                        tracker++;
+                        break;
+                    }
+                    else break;
                 }
-                else {
-                    if (! broke) final_pos = i - j;
-                    broke  = true;
-                    visible = false;
-                }
-
-                if (stoi(string(1,r_word[j])) > (stoi(string(1,r_word[i])))) {
-                    visible_r = true;
-                    if (! broke_r) final_pos_r = i - j;
-                }
-                else {
-                    if (! broke_r) final_pos_r = i - j;
-                    broke_r = true;
-                    visible_r = false;
-                }  
-
-                if (stoi(string(1,word_c[j])) > (stoi(string(1,word_c[i])))) {
-                    visible_c = true;
-                    if (! broke_c) final_pos_c = i - j;
-                }
-                else {
-                    if (! broke_c) final_pos_c = i - j;
-                    broke_c = true;
-                    visible_c = false;
-                }   
-
-                if (stoi(string(1,r_word_c[j])) > (stoi(string(1,r_word_c[i])))) {
-                    visible_c_r = true;
-                    if (! broke_c_r) final_pos_c_r = i - j;
-                }
-                else {
-                    if (! broke_c_r) final_pos_c_r = i - j;
-                    broke_c_r = true;
-                    visible_c_r = false;
-                }   
+                score *= tracker;
+                scores.emplace_back(score);
             }
-            matrix2[cnt][j] *= final_pos;
-            matrix2[cnt][pos] *= final_pos_r;
-            matrix2[j][cnt] *= final_pos_c;
-            matrix2[pos][cnt] *= final_pos_c_r;
-            if (visible && ! broke) matrix[cnt][j] += 1;
-            if (visible_r && ! broke_r) matrix[cnt][pos] += 1;
-            if (visible_c && ! broke_c) matrix[j][cnt] += 1;
-            if (visible_c_r && ! broke_c_r) matrix[pos][cnt] += 1;
-            --pos;
-        }
-        ++cnt;
-    }
-
-    int ttl {0};
-    for (auto i: matrix) {
-        for (auto j: i) {
-            if (j > 1) ++ttl;
         }
     }
-
-    int best {0};
-    for (auto i: matrix2) {
-        for (auto j: i) {
-            if (j >= best) best = j;
-        }
-    }
-
-    cout << "Part 1: " << ttl + len * 4  - 4 << '\n' << "Part 2: " << best << '\n';
+    
+    std::print("Part 1: {}\nPart 2: {}\n", count, *std::max_element(scores.begin(), scores.end()));
 }
