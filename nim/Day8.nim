60bc9533dd6d1b5aadc892bd80d523dffce5e5d7
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-07-24 13:07:30 -0600

    Add files via upload

---
+++ nim/Day8.nim
@@ -0,0 +1,60 @@
+import strformat
+
+proc readInput(): seq[string] =
+    var s: seq[string]
+    let file = open("input/Day8.txt");
+    defer: file.close()
+
+    for i in file.lines():
+        s.add(i)
+    
+    return s
+        
+let s = readInput()
+
+block:
+    var
+        count = (len(s) * 4) - 4
+        scores: seq[int]
+
+    for rows in 1..<len(s) - 1:
+        for cols in 1..<len(s) - 1:
+            var pos = int(s[rows][cols])
+
+            var
+                left: seq[int]
+                right: seq[int]
+                top: seq[int]
+                bottom: seq[int] 
+
+            for i in 1..<cols + 1: 
+                left.add(int(s[rows][cols - i]))
+
+            for i in 1..<len(s) - cols: 
+                right.add(int(s[rows][cols + i]))
+
+            for i in 1..<rows + 1: 
+                top.add(int(s[rows - i][cols]))
+                
+            for i in 1..<len(s) - rows: 
+                bottom.add(int(s[rows + i][cols]))
+
+            if max(left) < pos or max(right) < pos or max(top) < pos or max(bottom) < pos:
+                count += 1
+
+            var score = 1
+            for lst in [left, right, top, bottom]:
+                var tracker = 0
+                for i in 0..<len(lst):
+                    if lst[i] < pos:
+                        tracker += 1
+                    elif lst[i] == pos:
+                        tracker += 1
+                        break
+                    else:
+                        break
+            
+                score *= tracker
+                scores.add(score)
+    
+    echo &"Solution 1: {count}\nSolution 2: {max(scores)}"
