81d2a1dc279492d00342d80d5298a096c28acfac
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-08-09 21:54:56 -0600

    Update Day4.v

--- v/Day4.v
+++ v/Day4.v
@@ -2,19 +2,23 @@ import os
 
 fn main() {
 	mut arr := [][]int{}
-	mut lines := os.read_lines("input/Day4_2022.txt")!
-	for i in 0..lines.len {
-		lines[i] =lines[i].replace("-", ",")
-		arr << lines[i].split(",").map(it.int())
+	mut lines := os.read_lines('input/Day4_2022.txt')!
+	for i in 0 .. lines.len {
+		lines[i] = lines[i].replace('-', ',')
+		arr << lines[i].split(',').map(it.int())
 	}
 	mut ttl1, mut ttl2 := 0, 0
-        for i in arr {
-            if ((i[0] >= i[2] && i[0] <= i[3]) && (i[1] >= i[2] && i[1] <= i[3])) || 
-                ((i[2] >= i[0] && i[2] <= i[1]) && (i[3] >= i[0] && i[3] <= i[1])) {ttl1++}
-            
-            if ((i[0] >= i[2] && i[0] <= i[3]) || (i[1] >= i[2] && i[1] <= i[3])) || 
-                ((i[2] >= i[0] && i[2] <= i[1]) || (i[3] >= i[0] && i[3] <= i[1])) {ttl2++}
+	for i in arr {
+		if ((i[0] >= i[2] && i[0] <= i[3]) && (i[1] >= i[2] && i[1] <= i[3]))
+			|| ((i[2] >= i[0] && i[2] <= i[1]) && (i[3] >= i[0] && i[3] <= i[1])) {
+			ttl1++
 		}
-	
-	println("Part 1: ${ttl1}\nPart 2: ${ttl2}")	
+
+		if ((i[0] >= i[2] && i[0] <= i[3]) || (i[1] >= i[2] && i[1] <= i[3]))
+			|| ((i[2] >= i[0] && i[2] <= i[1]) || (i[3] >= i[0] && i[3] <= i[1])) {
+			ttl2++
+		}
+	}
+
+	println('Part 1: ${ttl1}\nPart 2: ${ttl2}')
 }
