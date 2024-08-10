d7ea1ee8b6ecd76dcf756fe4a088cfb6330e41c2
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-08-09 22:09:42 -0600

    Update tools.h

--- c++/tools.h
+++ c++/tools.h
@@ -9,6 +9,7 @@
 #include <ostream>
 #include <sstream>
 #include <memory>
+#include <math.h>
 
 typedef std::vector<int> vi;
 typedef std::vector<std::vector<int>> vvi;
@@ -31,20 +32,6 @@ std::ostream &operator<<(std::ostream &os, const std::vector<T> &arr) {
     os << "]\n";
     return os;
 }
-
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const std::tuple<T> &arr) {
-    os << "[";
-    for (size_t i {0}; i < arr.size(); ++i) {
-        os << arr[i];
-        if (i < arr.size() - 1) {
-            os << ",";
-        }
-    }
-    os << "]\n";
-    return os;
-}
-
 
 template <typename T>
 float avgVal(const std::vector<T> &arr) {
@@ -80,7 +67,7 @@ T join(const std::vector<T> &arr, const char delim = ',') {
     return str;
 }
 
-vi split(const std::string &s, const char del = ',') {
+std::vector<int> split(const std::string &s, const char del = ',') {
     vi result {};
     std::stringstream ss(s);
     std::string word;
@@ -91,7 +78,7 @@ vi split(const std::string &s, const char del = ',') {
     return result;
 }
 
-vi split(const std::vector<std::string> &arr, const char del = ',') {
+std::vector<int> split(const std::vector<std::string> &arr, const char del = ',') {
     vi result {};
 
     for (auto i: arr) {
@@ -103,6 +90,44 @@ vi split(const std::vector<std::string> &arr, const char del = ',') {
         }
     }
     return result;
+}
+
+template <typename T>
+std::vector<T> operator+(const std::vector<T> &arr, const std::vector<T> &arr2) {
+    std::vector<T> result {};
+    for (size_t i {0}; i < arr.size(); i++) {
+        result.push_back(arr[i] + arr2[i]);
+    }
+    return result;
+}
+
+template <typename T>
+T arrValue(const std::vector<std::vector<T>> &arr, const std::vector<int> &arr2) {
+    return arr[arr2[0]][arr2[1]];
+}
+
+template <typename T>
+struct Nbrs {
+    std::vector<std::vector<T>> indices {};
+    std::vector<int> vals {};
+};
+
+template <typename T>
+Nbrs<T> nbrs(const std::vector<std::vector<T>> &arr, const std::vector<int> &loc, bool diag = false) {
+    vvi dir = {};
+    if (!diag) dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    else dir = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    std::vector<T> vals {};
+    vvi indices {};
+    
+    for (const auto  &i : dir) {
+        auto tmp = loc + i;
+        if (tmp[0] != -1 && tmp[1] != -1 && (size_t) tmp[0] != arr.size() && (size_t) tmp[1] != arr[0].size()) {
+            indices.emplace_back(tmp);
+            vals.emplace_back(arrValue(arr, tmp));
+        }
+    }
+    return {indices, vals};
 }
 
 #endif
