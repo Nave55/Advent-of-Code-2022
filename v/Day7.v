7d17d4f51e5e10cc43a9fa21a8d075fe499b63d8
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-08-09 21:53:16 -0600

    Update Day7.v

--- v/Day7.v
+++ v/Day7.v
@@ -7,26 +7,34 @@ struct Directory {
 }
 
 fn main() {
-	lines := os.read_lines("input/Day7_2022.txt")!
-	
+	lines := os.read_lines('input/Day7_2022.txt')!
+
 	mut tmp := []string{}
 	mut uniq_dir_names := []string{}
 	mut dir := []Directory{}
 
-	for line in lines {
-		if line[0..4] == "$ cd" && line[5] != `.` {
+	for line in lines[0..] {
+		if line[0..4] == '$ cd' && line[5] != `.` {
+			if line[5] == `/` {
+				tmp << 'root'
+			} else {
 				tmp << line[5..]
-				uniq_dir_names << tmp.join("-")
 			}
+			uniq_dir_names << tmp.join('/')
+		}
 		if line.len >= 6 && line[5] == `.` {
 			tmp.pop()
 		}
 		if line[0] >= 48 && line[0] <= 57 {
-			p := tmp.join("-")
-			s := line[..line.index(" ")?].int()
-			dir << Directory{path: p, size: s}
-		}		
+			p := tmp.join('/')
+			s := int(line[..line.index(' ')?].int())
+			dir << Directory{
+				path: p
+				size: s
+			}
+		}
 	}
+
 	mut ttl := []int{}
 	for i in uniq_dir_names {
 		mut t_sum := 0
@@ -38,8 +46,7 @@ fn main() {
 		ttl << t_sum
 	}
 
-	pt1 := arrays.sum(ttl.filter(it <= 100_000))!
-	pt2 := arrays.min(ttl.filter(it  >= ttl[0] - 40_000_000))!
-
-	println("Part 1: ${pt1}\nPart 2: ${pt2}")
+	pt1 := int(arrays.sum(ttl.filter(it <= 100_000))!)
+	pt2 := arrays.min(ttl.filter(it >= ttl[0] - 40_000_000))!
+	println('Part 1: ${pt1}\nPart 2: ${pt2}')
 }
