29e318ff5252fb8ea58af2e890ef7f57fb1ba2d7
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-11-20 21:50:14 -0700

    Update day1.rs

--- rust/day1.rs
+++ rust/day1.rs
@@ -1,34 +1,35 @@
 use itertools::Itertools;
 
 fn main() {
-    let input = std::fs::read_to_string("../inputs/day1.txt").unwrap();
-    let b = pt1(&input);
-    let c = pt2(&input);
-    println!("Part 1: {b}\nPart 2: {c}")
+    let input = std::fs::read_to_string("../inputs/day1.txt").expect("Failed to read file");
+    let part1_result = pt1(&input);
+    let part2_result = pt2(&input);
+    println!("Part 1: {}\nPart 2: {}", part1_result, part2_result);
 }
 
 fn pt1(input: &str) -> u32 {
     input
         .split("\r\n\r\n")
-        .map(|line| {
-            line.lines()
-                .filter_map(|s| s.parse::<u32>().ok())
+        .map(|group| {
+            group
+                .lines()
+                .filter_map(|line| line.parse::<u32>().ok())
                 .sum::<u32>()
         })
         .max()
-        .unwrap()
+        .unwrap_or(0)
 }
 
 fn pt2(input: &str) -> u32 {
     input
         .split("\r\n\r\n")
-        .map(|line| {
-            line.lines()
-                .filter_map(|s| s.parse::<u32>().ok())
+        .map(|group| {
+            group
+                .lines()
+                .filter_map(|line| line.parse::<u32>().ok())
                 .sum::<u32>()
         })
-        .sorted()
-        .rev()
+        .sorted_by(|a, b| b.cmp(a))
         .take(3)
-        .sum::<u32>()
+        .sum()
 }
