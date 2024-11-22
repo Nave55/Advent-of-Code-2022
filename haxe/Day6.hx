dbe0e47224709f28bf13f930af7bf9edcf688a41
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2024-11-22 01:32:04 -0700

    Update Day6.hx

--- haxe/Day6.hx
+++ haxe/Day6.hx
@@ -1,27 +1,22 @@
+import Tools;
 import StringTools.*;
 using hx.strings.Strings;
 
 class Day6 {
     static function main() {
-        var data = [for (i in sys.io.File.getContent('Advent Files_2022/Day6.txt').split('\n')) trim(i)].join('');
+        var data = [for (i in sys.io.File.getContent('Advent Files_2022/Day6.txt').split('\n')) trim(i)][0].split('');
         var pt1 = solution(data);
         var pt2 = solution(data, 14);
         Sys.println('Solution 1: $pt1\nSolution 2: $pt2');
     }
 
-    static function solution(arr: String, window = 4) {
-        var sli = arr.split('');
-        for (j in 0...sli.length - window + 1) {
-            var uniq = true;
-            var str = sli.slice(j, j + window).join('');
-            for (k in str.split('')) {
-                if (str.countMatches(k) > 1) {
-                    uniq = false;
-                    break;
-                }
-            } 
-            if (uniq) return j + window;            
-        }  
+    static function solution(arr: AS, window = 4) {
+        for (i in 0...arr.length - window + 1) {
+            var a = new Set<String>(arr.slice(i, i + window));
+            if (a++.length == window) {
+                return i + window;
+            }
+        }
         return 0;
     }
 }
